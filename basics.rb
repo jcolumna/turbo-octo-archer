8cf3e5ce9c2481b66c701d280b4c0477a7d35315
Joshua Columna <josh.columna@gmail.com>
2014-06-06 15:23:32 -0600

    Added Basics Assignment

---
+++ basics.rb
@@ -0,0 +1,56 @@
+#Basics
+
+#1
+'Joshua' + 'Columna'
+
+#2
+my_num = 1234
+
+thousands = my_num / 1000
+hundreds  = my_num % 1000 / 100
+tens      = my_num % 100 / 10
+ones      = my_num % 10
+
+#3
+my_hash = {:Tampopo => 1985,
+ :'Summer Wars' => 2009,
+ :'Super 8' => 2011,
+ :'Scott Pilgrim vs. The World' => 2010,
+ :'Castle in the Sky' => 1986,
+ :'Street Fighter II: The Animated Movie' => 1994}
+
+puts my_hash[:Tampopo]
+puts my_hash[:'Summer Wars']
+puts my_hash[:'Super 8']
+puts my_hash[:'Scott Pilgrim vs. The World']
+puts my_hash[:'Castle in the Sky']
+puts my_hash[:'Street Fighter II: The Animated Movie']
+
+#4
+dates = [1985, 2009, 2011, 2010, 1986, 1994]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+puts dates[5]
+
+#5
+f5 = 5*4*3*2*1
+f6 = 6*f5
+f7 = 7*f6
+f8 = 8*f7
+
+puts f5
+puts f6
+puts f7
+puts f8
+
+#6
+puts 3.14 * 3.14
+puts 9.81 * 9.81
+puts 0.33 * 0.33
+
+#7
+#The error tells me that I used the wrong kind of closing brace. 
+
